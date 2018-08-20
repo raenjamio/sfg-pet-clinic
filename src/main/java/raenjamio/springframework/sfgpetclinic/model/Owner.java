6d5f3b2d56c8714f96d46bfe1c39bead2f1abc63
rodrigoenjamio <rodrigo.enjamio@despegar.com>
2018-08-20 13:26:24 -0300

    #2 closes add pojo model

---
+++ src/main/java/raenjamio/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package raenjamio.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/raenjamio/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package raenjamio.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+	
+}
