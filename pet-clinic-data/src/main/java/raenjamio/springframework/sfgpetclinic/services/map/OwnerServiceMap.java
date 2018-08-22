15d0425451fcb5ea6e1261f557ad74d921929207
Rodrigo <raenjamio@gmail.com>
2018-08-21 22:34:51 -0300

    add index page

---
+++ pet-clinic-data/src/main/java/raenjamio/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,36 @@
+package raenjamio.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import raenjamio.springframework.sfgpetclinic.model.Owner;
+import raenjamio.springframework.sfgpetclinic.services.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long>{
+
+	@Override
+	public Set<Owner> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return super.findById(id);
+	}
+	
+	@Override
+	public Owner save(Owner object) {
+		return super.save(object.getId(), object);
+	}
+	
+	@Override
+	public void delete(Owner object) {
+		super.delete(object);
+	}
+	
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+	
+	
+}
