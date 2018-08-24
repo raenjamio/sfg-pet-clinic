44af2317706ef2ab91658595525b02ed976657cb
rodrigoenjamio <rodrigo.enjamio@despegar.com>
2018-08-23 23:33:18 -0300

    add list vet

--- pet-clinic-web/src/main/java/raenjamio/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/raenjamio/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package raenjamio.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import raenjamio.springframework.sfgpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping({"/owners"})
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"","/","/index", "/index.html"})
-	public String listOwners(){
+	public String listOwners(Model model){
+		
+		model.addAttribute("owners", ownerService.findAll());
+		
 		return "owners/index";
 	}
 }
